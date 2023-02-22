10bc603992f3594cc5c9e561d0998a0b8763913a
Nikolaykrasnov <crasnov.colya2000@yandex.ru>
2023-02-22 16:46:22 +0500

    Добавьте файлы проекта.

---
+++ Branch design.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("a=");
+	scanf_s("%i",&a);
+	printf("b=");
+	scanf_s("%i",&b);
+		if (a > b)
+		{
+			printf("A bolshe");
+	}
+		else
+		{
+			if (a == b)
+			{
+				printf("A=B");
+			}
+			else
+			{
+				printf("B bolshe");
+			}
+				
+			
+				
+			
+			
+		}
+}
